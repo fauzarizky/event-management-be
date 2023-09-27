a29dd3f7e791e1bc9fca9bdbbd6128f5f9329720
ThePageMode <131966946+thepagemode@users.noreply.github.com>
2023-09-27 15:03:44 +0700

    fix associate, add middleware

--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-<<<<<<< HEAD
     password: "Makanayam666!",
-=======
-    password: "anggara123",
->>>>>>> 23db65d56f26485c58951b34cb70573480c73ac5
     database: "db_mytix",
     host: "127.0.0.1",
     dialect: "mysql",
---
+++ middleware/auth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "ntar-pindah-ke-env";
+
+exports.validateToken = (req, res, next) => {
+  let token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({
+      ok: false,
+      message: "Token is not found",
+    });
+    return;
+  }
+  try {
+    token = token.split(" ")[1];
+    if (!token) {
+      res.status(401).json({
+        ok: false,
+        message: "Token is not found",
+      });
+      return;
+    }
+
+    const payload = jwt.verify(token, JWT_SECRET_KEY);
+    if (!payload) {
+      res.status(401).json({
+        ok: false,
+        message: "Failed to get authorization data",
+      });
+      return;
+    }
+
+    req.user = payload;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      message: String(error),
+    });
+  }
+};
