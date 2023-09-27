cfe312b294da04f1de60a3dce45c2817c6c545b6
anggaratriputra <anggaratriputra@gmail.com>
2023-09-27 14:38:55 +0700

    change code transaction

--- models/transaction.js
+++ models/transaction.js
@@ -16,20 +16,6 @@ module.exports = (sequelize, DataTypes) => {
       Transaction.belongsTo(models.referral, { foreignKey: "referralId" });
     }
   }
-<<<<<<< HEAD
-  Transaction.init({
-    eventId: DataTypes.INTEGER,
-    accountId: DataTypes.INTEGER,
-    transactionDate: DataTypes.DATE,
-    paymentMethodId: DataTypes.INTEGER,
-    referralId: DataTypes.INTEGER,
-    couponId: DataTypes.INTEGER,
-    totalPrice: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Transaction',
-  });
-=======
   Transaction.init(
     {
       eventId: DataTypes.INTEGER,
@@ -49,6 +35,5 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Transaction",
     }
   );
->>>>>>> 3554ba0de7948f10e03a56fd7c40ff22b61f2cd2
   return Transaction;
 };
