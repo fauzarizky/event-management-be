66984c47ece46777ab5b8e8ac8aa9ea7f9a12ed2
fauzarizky <fauzasmg@gmail.com>
2023-09-28 20:35:27 +0700

    modified event models,migrations, controller

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "anggara123",
+    password: "fauza2023",
     database: "db_mytix",
     host: "127.0.0.1",
     dialect: "mysql",
--- models/event.js
+++ models/event.js
@@ -3,18 +3,22 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
     static associate(models) {
+      // This should be 1 to M relation
     }
   }
   Event.init(
     {
       name: DataTypes.CHAR(100),
+      accountId: DataTypes.INTEGER,
       type: DataTypes.ENUM("music", "webinar", "sports"),
       date: DataTypes.DATE,
       location: DataTypes.ENUM("online", "jakarta", "bekasi", "surabaya", "lombok", "bali", "lampung", "malaysia"),
       description: DataTypes.CHAR,
       gold_ticket_price: DataTypes.INTEGER,
       platinum_ticket_price: DataTypes.INTEGER,
-      diamond_ticket_price: DataTypes.INTEGER
+      diamond_ticket_price: DataTypes.INTEGER,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
     },
     {
       sequelize,
