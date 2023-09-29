730dd1eb0218cfa4bdff8c5cb674f45f318be144
anggaratriputra <anggaratriputra@gmail.com>
2023-09-29 15:28:28 +0700

    add validation role

--- routes/event.js
+++ routes/event.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const eventController = require("../controller/event");
-const authMiddlewere = require("../middleware/auth");
+const authMiddleware = require("../middleware/auth");
 const { multerUpload } = require("../lib/multer");
 
 router.get("/", eventController.getAllEvents);
 router.get("/:event", eventController.getSpesificEvent);
 router.get("/location/:location", eventController.getEventByLocation);
-router.post("/create", multerUpload.single("img"), eventController.handleCreateEvent);
+router.post("/create", authMiddleware.validateToken, authMiddleware.checkRole, multerUpload.single("img"), eventController.handleCreateEvent);
 
 module.exports = router;
